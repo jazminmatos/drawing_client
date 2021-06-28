d686e2ae51920dfb9f03419b6f1c2077e305bc38
jazminmatos <jmatos1993@gmail.com>
2021-06-28 15:49:12 -0400

    added click circle button event listener

--- src/drawing.js
+++ src/drawing.js
@@ -4,12 +4,15 @@ const canvas = document.getElementById('canvas');
 // leads to creation of a CanvasRenderingContext2d object
 // which represents a 2d rendering context
 const ctx = canvas.getContext("2d");
-
-// Resizing
 // Allows canvas size to be it's container's size
 const drawingBoard = document.getElementsByClassName('drawing-board');
 canvas.height = drawingBoard[0].offsetHeight;
 canvas.width = drawingBoard[0].offsetWidth;
+// Buttons
+const lineButton = document.querySelector("body > main > section > div.dashboard > button:nth-child(1)")
+const circleButton = document.querySelector("body > main > section > div.dashboard > button:nth-child(2)");
+const triangleButton = 
+const squareButton = 
 
 // local variables
 let drawing = false;
@@ -40,35 +43,40 @@ class Circle {
     }
 }
 
-// Allows user to click and have a circle appear
-// HOWEVER, if user resizes the viewport, mouseposition vs circle position changes...
-canvas.addEventListener("mousedown", function(e) {
-    drawing = true
-    // Retrieve position of canvas
-    // returns DOMRect object w/ info about the size of an element & its position relative to the viewport
-    // Had to move this inside this eventListener b/c as global variables, whenever viewport = resized, mouseposition/circle changed
-    let canvasBounds = canvas.getBoundingClientRect();
-    let rectLeft = canvasBounds.left;
-    let rectTop = canvasBounds.top
 
-    let newCircle = new Circle(e.clientX - rectLeft, e.clientY - rectTop, 50, "red");
-    newCircle.draw(ctx)    
+// EventListeners
+// When circleButton clicked, user can draw w/ circles
+circleButton.addEventListener("click", function(e) {
+    // Allows user to click and have a circle appear
+    // HOWEVER, if user resizes the viewport, mouseposition vs circle position changes...
+    canvas.addEventListener("mousedown", function(e) {
+        drawing = true
+        // Retrieve position of canvas
+        // returns DOMRect object w/ info about the size of an element & its position relative to the viewport
+        // Had to move this inside this eventListener b/c as global variables, whenever viewport = resized, mouseposition/circle changed
+        let canvasBounds = canvas.getBoundingClientRect();
+        let rectLeft = canvasBounds.left;
+        let rectTop = canvasBounds.top
+
+        let newCircle = new Circle(e.clientX - rectLeft, e.clientY - rectTop, 50, "red");
+        newCircle.draw(ctx)    
+    })
+
+    // If I am not drawing, i.e. if I didn't do 'mousedown', nothing will happen when I move the mouse  
+    canvas.addEventListener("mousemove", function(e) {
+        if (!drawing) return;
+        let canvasBounds = canvas.getBoundingClientRect();
+        let rectLeft = canvasBounds.left;
+        let rectTop = canvasBounds.top
+
+        let newCircle = new Circle(e.clientX - rectLeft, e.clientY - rectTop, 50, "red");
+        newCircle.draw(ctx)  
+    })
+
+    canvas.addEventListener("mouseup", function(e) {
+        drawing = false
+    })
 })
-
-// maybe I need to first check whether I have clicked i.e. drawing = true 
-canvas.addEventListener("mousemove", function(e) {
-    if (!drawing) return;
-    let canvasBounds = canvas.getBoundingClientRect();
-    let rectLeft = canvasBounds.left;
-    let rectTop = canvasBounds.top
-
-    let newCircle = new Circle(e.clientX - rectLeft, e.clientY - rectTop, 50, "red");
-    newCircle.draw(ctx)  
-})
-
-canvas.addEventListener()
-
-
 
 
 // figure out mouse/circle position issue
