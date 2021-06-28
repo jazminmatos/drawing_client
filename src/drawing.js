b9307f4f33421cf4e71cbd15c8c23060901c2a18
jazminmatos <jmatos1993@gmail.com>
2021-06-27 22:43:41 -0400

    add functions & event listeners

--- src/drawing.js
+++ src/drawing.js
@@ -4,3 +4,34 @@ const canvas = document.querySelector("#canvas");
 // leads to creation of a CanvasRenderingContext2d object
 // which represents a 2d rendering context
 const ctx = canvas.getContext("2d");
+
+// local variables
+let drawing = false;
+
+// functions
+function startPosition() {
+    drawing = true;
+}
+
+function endPosition() {
+    drawing = false;
+}
+
+function draw(e) {
+    // if we're not drawing, return 
+    if (!drawing) return;
+    ctx.lineWidth = 10;
+    ctx.lineCap = "round";
+
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+}
+
+// EventListeners
+// whenever we put mousedown, painting = true
+canvas.addEventListener('mousedown', startPosition)
+// whenever we put mouseup, painting = false
+canvas.addEventListener('mouseup', endPosition)
+canvas.addEventListener('mousemove', draw)
+
+}
