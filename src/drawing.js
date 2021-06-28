3e7ec7b42d7d4d4c29c8a5d14e559b3a79655cab
jazminmatos <jmatos1993@gmail.com>
2021-06-27 22:32:58 -0400

    successfully sized canvas height

--- src/drawing.js
+++ src/drawing.js
@@ -1,3 +1,6 @@
-window.addEventListener("load", () => {
-    console.log("Hello World")
-})
+// Global variables, move to index.js?
+
+const canvas = document.querySelector("#canvas");
+// leads to creation of a CanvasRenderingContext2d object
+// which represents a 2d rendering context
+const ctx = canvas.getContext("2d");
