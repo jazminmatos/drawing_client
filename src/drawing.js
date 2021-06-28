6b64be7f08d6bf36b4fcd78603a040c19201887a
jazminmatos <jmatos1993@gmail.com>
2021-06-27 23:01:40 -0400

    removed functions & eventlisteners

--- src/drawing.js
+++ src/drawing.js
@@ -5,33 +5,8 @@ const canvas = document.getElementById('canvas');
 // which represents a 2d rendering context
 const ctx = canvas.getContext("2d");
 
-ctx.fillRect(0, 1, 100, 100)
+ctx.fillRect(800, 27, 100, 100)
 
 // local variables
 let drawing = false;
 
-// functions
-function startPosition() {
-    drawing = true;
-}
-
-function endPosition() {
-    drawing = false;
-}
-
-function draw(e) {
-    // if we're not drawing, return 
-    if (!drawing) return;
-    ctx.lineWidth = 10;
-    ctx.lineCap = "round";
-
-    ctx.lineTo(e.clientX, e.clientY);
-    ctx.stroke();
-}
-
-// EventListeners
-// whenever we put mousedown, painting = true
-canvas.addEventListener('mousedown', startPosition)
-// whenever we put mouseup, painting = false
-canvas.addEventListener('mouseup', endPosition)
-canvas.addEventListener('mousemove', draw)
