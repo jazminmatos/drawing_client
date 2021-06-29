be7dfe1e8db4ed56aa1fd2a4f30fbd9d2f4630a7
jazminmatos <jmatos1993@gmail.com>
2021-06-29 13:51:20 -0400

    no luck w/ refactoring

--- src/line.js
+++ src/line.js
@@ -13,29 +13,6 @@ class Line {
         context.beginPath()
         context.moveTo(this.xpos, this.ypos);
     }
-
-/////////////////////////////////////////////////////////
-
-    // startDrawing(e) {
-    //     drawing = true;
-    //     draw(e);
-    // }
-
-    // draw(e) {
-    //     if (!drawing) return;
-    //     ctx.lineWidth = 10;
-    //     ctx.lineCap = "round";
-        
-    //     ctx.lineTo(e.clientX, e.clientY);
-    //     ctx.stroke();
-    //     ctx.beginPath()
-    //     ctx.moveTo(this.xpos, this.ypos);
-    // }
-
-//     stopDrawing(e) {
-//         drawing = false;
-//         //ctx.beginPath();
-//     }
 }
 
 // EventListeners
@@ -72,4 +49,3 @@ lineButton.addEventListener("click", function(e) {
 clearButton.addEventListener("click", function(e) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 })
-
