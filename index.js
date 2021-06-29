ae93c3bdd26b1a17c8f8eb263857e3c8ba71cdfa
jazminmatos <jmatos1993@gmail.com>
2021-06-29 13:37:36 -0400

    cleaned up files

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const ctx = canvas.getContext("2d");
 const drawingBoard = document.getElementsByClassName('drawing-board');
 canvas.height = drawingBoard[0].offsetHeight;
 canvas.width = drawingBoard[0].offsetWidth;
+
 // Buttons
 const saveButton = document.getElementById('save-button');
 
@@ -16,75 +17,5 @@ let drawing = false;
 let canvasBounds = canvas.getBoundingClientRect();
 let rectLeft = canvasBounds.left;
 let rectTop = canvasBounds.top;
-
-// EventListeners
-// When circleButton clicked, user can draw w/ circles
-// circleButton.addEventListener("click", function(e) {
-//     // Allows user to click and have a circle appear
-//     // HOWEVER, if user resizes the viewport, mouseposition vs circle position changes...
-//     canvas.addEventListener("mousedown", function(e) {
-//         drawing = true
-//         // Retrieve position of canvas
-//         // returns DOMRect object w/ info about the size of an element & its position relative to the viewport
-//         // Had to move this inside this eventListener b/c as global variables, whenever viewport = resized, mouseposition/circle changed
-//         let canvasBounds = canvas.getBoundingClientRect();
-//         let rectLeft = canvasBounds.left;
-//         let rectTop = canvasBounds.top
-
-//         let newCircle = new Circle(e.clientX - rectLeft, e.clientY - rectTop, 50, "red");
-//         newCircle.draw(ctx)    
-//     })
-
-//     // If I am not drawing, i.e. if I didn't do 'mousedown', nothing will happen when I move the mouse  
-//     canvas.addEventListener("mousemove", function(e) {
-//         if (!drawing) return;
-//         let canvasBounds = canvas.getBoundingClientRect();
-//         let rectLeft = canvasBounds.left;
-//         let rectTop = canvasBounds.top
-
-//         let newCircle = new Circle(e.clientX - rectLeft, e.clientY - rectTop, 50, "red");
-//         newCircle.draw(ctx)  
-//     })
-
-//     canvas.addEventListener("mouseup", function(e) {
-//         drawing = false
-//     })
-// })
-
-// EventListeners
-// When lineButton clicked, user can draw w/ lines
-// An example of a permanent 'line' (looks like a dot):
-// let line = new Line(100, 200)
-// line.draw(ctx)
-// lineButton.addEventListener("click", function(e) {
-//     canvas.addEventListener("mousedown", function(e) {
-//         drawing = true
-//         let canvasBounds = canvas.getBoundingClientRect();
-//         let rectLeft = canvasBounds.left;
-//         let rectTop = canvasBounds.top;
-        
-//         let line = new Line (e.clientX - rectLeft, e.clientY - rectTop);
-//         line.draw(ctx);
-//     })
-    
-//     canvas.addEventListener("mousemove", function(e) {
-//         if (!drawing) return;
-//         let canvasBounds = canvas.getBoundingClientRect();
-//         let rectLeft = canvasBounds.left;
-//         let rectTop = canvasBounds.top;
-        
-//         let line = new Line (e.clientX - rectLeft, e.clientY - rectTop);
-//         line.draw(ctx);
-//     })
-
-//     canvas.addEventListener("mouseup", function(e) {
-//         drawing = false
-//         ctx.beginPath()
-//     })
-// })
-
-// clearButton.addEventListener("click", function(e) {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-// })
 
 // Fetch Requests
--- src/circle.js
+++ src/circle.js
@@ -1,109 +1,61 @@
-class Circle {
-    constructor(xpos, ypos, radius, color) {
-        this.xpos = xpos;
-        this.ypos = ypos;
-        this.radius = radius;
-        this.color = color;
-    }
+// class Circle {
+//     constructor(xpos, ypos, radius, color) {
+//         this.xpos = xpos;
+//         this.ypos = ypos;
+//         this.radius = radius;
+//         this.color = color;
+//     }
 
-    // input will be ctx, defined in index.js
-    draw(context) {
-        // creates a new path
-        // creates the circle
-        context.beginPath();
-        context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI*2, false);
-        // outlines the current or given path w/ current stroke style
-        context.stroke(); 
-        context.closePath();
-    }
-}
-
-//Buttons
-const circleButton = document.getElementById('circle-button');
-
-circleButton.addEventListener("click", function(e) {
-    // Allows user to click and have a circle appear
-    // HOWEVER, if user resizes the viewport, mouseposition vs circle position changes...
-    canvas.addEventListener("mousedown", function(e) {
-        drawing = true
-        // Retrieve position of canvas
-        // returns DOMRect object w/ info about the size of an element & its position relative to the viewport
-        // Had to move this inside this eventListener b/c as global variables, whenever viewport = resized, mouseposition/circle changed
-        let canvasBounds = canvas.getBoundingClientRect();
-        let rectLeft = canvasBounds.left;
-        let rectTop = canvasBounds.top
-
-        let newCircle = new Circle(e.clientX - rectLeft, e.clientY - rectTop, 50, "red");
-        newCircle.draw(ctx)    
-    })
-
-    // If I am not drawing, i.e. if I didn't do 'mousedown', nothing will happen when I move the mouse  
-    canvas.addEventListener("mousemove", function(e) {
-        if (!drawing) return;
-        let canvasBounds = canvas.getBoundingClientRect();
-        let rectLeft = canvasBounds.left;
-        let rectTop = canvasBounds.top
-
-        let newCircle = new Circle(e.clientX - rectLeft, e.clientY - rectTop, 50, "red");
-        newCircle.draw(ctx)  
-    })
-
-    canvas.addEventListener("mouseup", function(e) {
-        drawing = false
-    })
-})
-
-// An example of a permanent static circle:
-// let circle = new Circle(100, 100, 50, "black");
-// Need to call draw in order for it to show up
-//circle.draw(ctx)
-
-// ----------------------------------------------------------------------------------------------------
-
-
-// class Drawing {
-    // do I need user_id or is that relationship created using the backend only?
-    // constructor(image) {
-    //     this.image = image
-    // }
-
-    // function that tells me I am drawing
-    // isDrawing() {
-        // drawing = true;
-    // }
-
-    // function to be able to draw
-    // draw(e) {
-        // if (!isDrawing) return;
-        // ctx.lineWidth = 10;
-        // ctx.lineCap = "round"
-
-        // ctx.lineTo(e.clientX, e.clientY);
-        // what allows us to draw a line
-        // ctx.stroke();
-        // ctx.beginPath();
-        // ctx.moveTo(e.clientX, e.clientY);
-    // }
-
-    // function to clear the canvas
-
-    // function to convert Drawing instance into image using canvas.toDataURL()
+//     // input will be ctx, defined in index.js
+//     draw(context) {
+//         // creates a new path
+//         // creates the circle
+//         context.beginPath();
+//         context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI*2, false);
+//         // outlines the current or given path w/ current stroke style
+//         context.stroke(); 
+//         context.closePath();
+//     }
 // }
 
-// EventListeners
-// // when mouse is clicked, drawing = true
-// canvas.addEventListener("mousedown", isDrawing)
+// //Buttons
+// const circleButton = document.getElementById('circle-button');
 
-// // when mouse is NOT clicked, drawing = false
-// canvas.addEventListener("mouseup", isNOTDrawing)
+// // EventListeners
+// // When circleButton clicked, user can draw w/ circles
+// circleButton.addEventListener("click", function(e) {
+//     // Allows user to click and have a circle appear
+//     // HOWEVER, if user resizes the viewport, mouseposition vs circle position changes...
+//     canvas.addEventListener("mousedown", function(e) {
+//         drawing = true
+//         // Retrieve position of canvas
+//         // returns DOMRect object w/ info about the size of an element & its position relative to the viewport
+//         // Had to move this inside this eventListener b/c as global variables, whenever viewport = resized, mouseposition/circle changed
+//         let canvasBounds = canvas.getBoundingClientRect();
+//         let rectLeft = canvasBounds.left;
+//         let rectTop = canvasBounds.top
 
-// // when mouse is moving, call draw method
-// canvas.addEventListener("mousemove", draw)
+//         let newCircle = new Circle(e.clientX - rectLeft, e.clientY - rectTop, 50, "red");
+//         newCircle.draw(ctx)    
+//     })
 
-// REMINDER: 4 diff types of class methods
-// 1. standard instance methods
-// 2. static (class-level) methods
-// 3. getter methods
-// 4. setter methods
-// All functions that I need will need to be instance methods...I think
+//     // If I am not drawing, i.e. if I didn't do 'mousedown', nothing will happen when I move the mouse  
+//     canvas.addEventListener("mousemove", function(e) {
+//         if (!drawing) return;
+//         let canvasBounds = canvas.getBoundingClientRect();
+//         let rectLeft = canvasBounds.left;
+//         let rectTop = canvasBounds.top
 
+//         let newCircle = new Circle(e.clientX - rectLeft, e.clientY - rectTop, 50, "red");
+//         newCircle.draw(ctx)  
+//     })
+
+//     canvas.addEventListener("mouseup", function(e) {
+//         drawing = false
+//     })
+// })
+
+// // An example of a permanent static circle:
+// // let circle = new Circle(100, 100, 50, "black");
+// // Need to call draw in order for it to show up
+// //circle.draw(ctx)
