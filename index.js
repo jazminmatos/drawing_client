52d288342b773fc8e6686ac78b15ff0d8df2122b
jazminmatos <jmatos1993@gmail.com>
2021-06-29 23:21:10 -0400

    cleaned up 1st fetch request

--- index.js
+++ index.js
@@ -27,13 +27,14 @@ let rectTop = canvasBounds.top;
 // Fetch Requests
 
 // Fetch (GET) from JSON file
-fetch(jsonImageURL).then(response =>
-        response.json()
-    ).then(function(json) {
-        let keys = Object.keys(json);
-        console.log(json[keys[keys.length * Math.random() << 0]])
-        // use data inside of json to do DOM manipulation
-    })
+fetch(jsonImageURL)
+    .then(response =>response.json())
+    .then(function(json) {
+    let keys = Object.keys(json);
+    let imageURL = json[keys[keys.length * Math.random() << 0]].image;
+    console.log(imageURL)
+    // use data inside of json to do DOM manipulation
+})
     // use the Math.random - # between 0-16 - to get an item
     // in the object, then get its image property
 
