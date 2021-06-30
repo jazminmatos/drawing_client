5f1640b2640a5e476eb4083da5143e82339261e5
jazminmatos <jmatos1993@gmail.com>
2021-06-30 00:01:50 -0400

    developing getImage method

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const drawingBoard = document.getElementsByClassName('drawing-board');
 canvas.height = drawingBoard[0].offsetHeight;
 canvas.width = drawingBoard[0].offsetWidth;
 
+let drawing = false;
+
+let canvasBounds = canvas.getBoundingClientRect();
+let rectLeft = canvasBounds.left;
+let rectTop = canvasBounds.top;
+
 const colorPicker = document.querySelector("body > main > section > div.dashboard > section.colors > input");
 const strokeWeight = document.querySelector("body > main > section > div.dashboard > section.thickness > input");
 
@@ -15,20 +21,10 @@ const strokeWeight = document.querySelector("body > main > section > div.dashboa
 const saveButton = document.getElementById('save-button');
 
 // Local variables
-let drawing = false;
 
-let canvasBounds = canvas.getBoundingClientRect();
-let rectLeft = canvasBounds.left;
-let rectTop = canvasBounds.top;
 
 // Fetch Requests
-
-
 // Fetch (GET) drawing from backend db
-
 // Fetch (POST) drawing (send to backend db)
-
-
-
 // Not sure if I want to do this one 
 // Fetch (GET) all drawings from backend db
--- src/referenceimage.js
+++ src/referenceimage.js
@@ -1,21 +1,37 @@
-// Fetch (GET) from JSON file
-fetch(jsonImageURL)
-    .then(response =>response.json())
-    .then(function(json) {
-    let keys = Object.keys(json);
-    let imageURL = json[keys[keys.length * Math.random() << 0]].image;
-    console.log(imageURL)
-    // use data inside of json to do DOM manipulation
-})
-    // use the Math.random - # between 0-16 - to get an item
-    // in the object, then get its image property
-
-    // DOM manipulation: append img src element using retrieved URL
-
-    const jsonImageURL = "http://localhost:8000/images"
+const jsonImageURL = "http://localhost:8000/images"
 
 class ReferenceImage {
     constructor (endpoint) {
         this.endpoint = endpoint
     }
+
+    // Read/Index Action
+    getImage () {
+        fetch(`{this.endpoint}images`)
+            .then(resp => resp.json)
+            .then(function(json) {
+            let keys = Object.keys(json);
+            let imageURL = json[keys[keys.length * Math.random() << 0]].image;
+            console.log(imageURL)
+            // use data inside of json to do DOM manipulation
+            })
+    }    
 }
+
+// // Fetch (GET) from JSON file
+// fetch(jsonImageURL)
+//     .then(response =>response.json())
+//     .then(function(json) {
+//     let keys = Object.keys(json);
+//     let imageURL = json[keys[keys.length * Math.random() << 0]].image;
+//     console.log(imageURL)
+//     // use data inside of json to do DOM manipulation
+// })
+// use the Math.random - # between 0-16 - to get an item
+// in the object, then get its image property
+
+// DOM manipulation: append img src element using retrieved URL
+
+
+const base_image_url = "http://localhost:8000/"
+const referenceImage = new ReferenceImage(base_image_url)
