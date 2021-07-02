224b7769242c62bc522764681699a142fec99519
jazminmatos <jmatos1993@gmail.com>
2021-07-01 20:28:34 -0400

    cleaned up index.js

--- index.js
+++ index.js
@@ -34,62 +34,10 @@ const userService = new UserService(base_rails_url);
 
 User.userSubmitButton.addEventListener ("click", function(e) {
     let userExists = User.all.find(userInstance => userInstance.username === User.userInput.value);
+    // Necessary to check User.all length b/c User.all array becomes empty on refresh
     if (User.all.length > 0 && userExists != undefined) {
         userExists.putOnDom()
     } else {
         userService.getUser()
     }
-    // // If page has been reloaded, User.all will be empty
-    // // If User.all is empty, then we have to run getUser (fetch GET request)
-    // if (User.all.length === 0) {
-    //     userService.getUser()
-    //     // Check if the inputValue === to object in JSON object
-    //         // if exists in JSON Obj & User.all is empty...
-    //             // create a new instance of the User (in the frontend)
-    //             // append that to the DOM
-    //         // if it doesn't exist in the JSON Obj
-    //             // run createUser (wrote this.createUser() in getUser() method)
-    // // If User.all is NOT empty
-    // } else {
-    //     // Check if inputValue exists in User.all
-    //     let userExists = User.all.find(userInstance => userInstance.username === User.userInput.value);
-    //     // if inputValue exists in User.all
-    //     if (userExists != undefined) {
-    //         // use that user instance and put it on the DOM
-    //         userExists.putOnDom()
-    //     // if inputValue does NOT exist in User.all
-    //     } else {
-    //         // Check if the inputValue === to object in JSON object
-    //         // if exists in JSON Obj & User.all is empty...
-    //             // create a new instance of the User (in the frontend)
-    //             // append that to the DOM
-    //         // if it doesn't exist in the JSON Obj
-    //             // run createUser (wrote this.createUser() in getUser() method)
-    //         userService.getUser()
-    //     }
-    // }
 })
-
-
-// if (User.all.length === 0) {
-//     userService.getUser()
-// } else {
-//     let userExists = User.all.find(userInstance => userInstance.username === User.userInput.value);
-//     if (userExists != undefined) {
-//         userExists.putOnDom()
-//     } else {
-//         userService.getUser()
-//     }
-// }
-
-// Need to Refactor:
-// If User.all != empty && inputValue exists in User.all --> userExists.putOnDom()
-// If User.all is empty --> userService.getUser()
-// If User.all is NOT empty BUT inputValue doesn't exist in backend --> userService.getUser()
-
-// let userExists = User.all.find(userInstance => userInstance.username === User.userInput.value);
-// if (User.all.length > 0 && userExists != undefined) {
-//     userExists.putOnDom()
-// } else {
-//     userService.getUser()
-// }
